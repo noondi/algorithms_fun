7856156e08387c6017a318e58db18defae53deca
root <root@localhost.localdomain>
2018-04-21 17:45:57 -0400

    4th commit

--- recursionFACTORIAL.js
+++ recursionFACTORIAL.js
@@ -1,11 +1,15 @@
+// Factorial(!)
+// 4! = 4 * 3 * 2 * 1 = 24
+// 5! = 5 * 4 * 3 * 2 * 1 = 120 
 function factorial(num){
     // base case
     if (num === 1 ){
         return num;
     } else {
+        // recursion case
         // the function that calls itself -  hence RECURSION
         // it will continue to call itself until the base is satisfied
         return num * factorial(num -1);
     }
 }
-factorial(4);
+factorial(5);
